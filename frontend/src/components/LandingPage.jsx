e9cd95f16bbe0f44c422c08df320ea88f53b1418
yashica1511 <mailtoyashi04@gmail.com>
2025-02-27 21:31:01 +0530

    Case Added

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -48,7 +48,7 @@ const LandingPage = () => {
                             <p className="text-base">Track your typing improvement over time with detailed statistics and graphs.</p>
                         </div>
                         <div className="bg-white text-gray-900 font-semibold p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-2xl duration-300 ease-in-out">
-                            <h3 className="text-xl font-semibold mb-4">Multiplayer Mode</h3>
+                            <h3 className="text-xl font-semibold mb-4">Game Mode</h3>
                             <p className="text-base">Enjoy fun typing games to challenge your speed and accuracy while having fun.</p>
                         </div>
                     </div>
